a6535786484f1ffaad6543f297b92afe1428db07
Isaac <isaaconyes80@gmail.com>
2024-03-25 16:11:25 +0100

    deleted media routes

--- Backend/controllers/payment.js
+++ Backend/controllers/payment.js
@@ -3,18 +3,16 @@ const stripe = require('stripe')(process.env.SECRET_KEY)
 const payWithStripe = async (req, res) => {
     try {
         const { amount, token } = req.body;
-
         const paymentIntent = await stripe.paymentIntents.create({
             amount,
             currency: 'usd',
             payment_method: token.id,
             confirm: true,
         });
-
-        res.json({ client_secret: paymentIntent.client_secret });
+        res.status(200).json({ status: 'Success', client_secret: paymentIntent.client_secret });
     } catch (error) {
         console.error(error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({success:"false", error: 'Internal Server Error' });
     }
 
 };
@@ -22,6 +20,5 @@ const payWithStripe = async (req, res) => {
 
 
 module.exports = {
-    payWithStripe,
-    payWithGPay
+    payWithStripe
 };
--- Backend/routes/media.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const cookieParser = require('cookie-parser')
-const {uploadProfilePic} = require('../controllers/media')
-const {mediaUploadProfile} = require('../config/multerConfig')
-router.use(cookieParser())
-
-router.route('/upload').post(mediaUploadProfile.single('file'), uploadProfilePic);
-
-module.exports = router
