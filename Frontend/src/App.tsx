8ddab9e57144e36f41f828c78abcd8cfb9319d95
ewan tomson <tomsonewan@gmail.com>
2024-03-24 16:00:30 -1100

    association

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Selected from './pages/Selected/Selected';
-import './App.css';
 import AllBingos from './pages/AllBingos/AllBingos';
 import Play from './pages/Play/Play';
 import Pay from './pages/Pay/Pay';
 import PaySuccess from './pages/PaySuccess/PaySuccess';
 import NotFound from './pages/NotFound/NotFound'; // Assume you have this component
+import Association from './pages/Association/Association'
+import bgImage from './assets/background_ball.png'
+
+import './App.css';
 
 const App = () => {
   const Layout1 = () => {
+    const location = useLocation();
+
     return (
-      <div className='w-full relative bg-blue-200 h-full overflow-y-auto'>
+      <div className="w-full relative bg-blue-200 h-full overflow-y-auto">
+        {
+          location.pathname === '/association' ?
+            <div className='absolute z-50 bg-transparent flex-1 text-center justify-center h-full w-5/12'>
+              <img src={bgImage} alt="Bingo" style={{height: "100%"}} />
+            </div> : <div></div>
+        }
         <div className='sticky container z-[1000] pt-[2rem] px-[8rem] top-0 left-0 w-full py-[1rem]'>
           <NavBar />
         </div>
@@ -33,6 +44,7 @@ const App = () => {
           <Route path="pay" element={<Pay />} />
           <Route path="pay-success" element={<PaySuccess />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="association" element={<Association />} />
         </Route>
       </Routes>
     </div>
