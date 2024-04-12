e1b70c209bc42724d9c2bb0367be3b8947f35c1e
Isaac <isaaconyes80@gmail.com>
2024-04-12 15:59:19 +0100

    updated routes frontend

--- Frontend/src/Routes/PrivateRoute/PrivateRoute.tsx
+++ Frontend/src/Routes/PrivateRoute/PrivateRoute.tsx
@@ -1,12 +1,24 @@
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
+import { ApiStatus } from '../../types/types';
+import Logo from '../../assets/solar_star-bold-duotone.svg'
 
-export const PrivateRoute = ({ element }:{element:React.ReactNode}) => {
-  const { user} = useAppSelector((state) => state.auth);
+export const PrivateRoute = ({ element }: { element: React.ReactNode }) => {
+  const { user, getRefreshStatus } = useAppSelector(state => state.auth)
+
+  if (getRefreshStatus === ApiStatus.loading || getRefreshStatus === ApiStatus.ideal) {
+    return <div className="w-full bg-gray-100 h-screen relative">
+      <span className="absolute h-[max-content] w-[max-content] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <div className="w-[5rem] rotateAndScale rounded-full h-[5rem]">
+          <img className="w-full h-full object-cover" src={Logo} alt="logo" />
+        </div>
+
+      </span>
+    </div>
+  }
 
   if (!user.accesstoken) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
   return element;
 };
-
--- Frontend/src/Routes/PublicRoute/PublicRoute.tsx
+++ Frontend/src/Routes/PublicRoute/PublicRoute.tsx
@@ -4,14 +4,16 @@ import Logo from '../../assets/solar_star-bold-duotone.svg'
 
 import { useAppSelector } from "../../app/hooks";
 export const PublicRoute = ({ element }: { element: React.ReactNode }) => {
-  const {user,getRefreshStatus} = useAppSelector(user=>user.auth)
+  const { user, getRefreshStatus } = useAppSelector(user => user.auth)
 
-  if(getRefreshStatus === ApiStatus.loading){
-    return <div className="w-full bg-gray-200 h-screen relative">
-           <span className="absolute h-[max-content] w-[max-content]  top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-           <img className="w-[5rem] h-[5rem]" src={Logo} alt="logo" />
-           </span>
-          </div>
+  if (getRefreshStatus === ApiStatus.loading) {
+    return <div className="w-full bg-gray-100 h-screen relative">
+      <span className="absolute h-[max-content] w-[max-content] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <div className="w-[5rem] rotateAndScale rounded-full h-[5rem]">
+          <img className="w-full h-full object-cover" src={Logo} alt="logo" />
+        </div>
+      </span>
+    </div>
   }
-    return user.accesstoken? <Navigate to='/'/> : element
-  };
+  return user.accesstoken ? <Navigate to='/' /> : element
+};
